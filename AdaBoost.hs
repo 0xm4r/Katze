2d7971e7414ad3880936a023912c3071caeb0b1e
Omar Abdeldayem <omar.abdeldayem@mail.utoronto.ca>
2015-09-05 00:10:24 -0400

    Added weight initialization and weak threshold classifier.

--- AdaBoost.hs
+++ AdaBoost.hs
@@ -1,3 +1,13 @@
 module AdaBoost where
 
-normalize n p = map( / (sum $ n ++ p)) $ n ++ p
+-- l == n
+initializeNegativeWeights l n = take n repeat (1 / (2 * l))
+
+-- m == p
+initializePositiveWeights m p = take p repeat(1 / (2 * p))
+
+normalizeWeights n p = map( / (sum $ n ++ p)) $ n ++ p
+
+thresholdClassifier threshold, feature, polarity = if polarity * feature < threshold
+                                                   then 1
+                                                   else 0

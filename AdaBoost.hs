22788115e50e0a59db1cc35ad5f874a2c230f5e9
Omar Abdeldayem <omar.abdeldayem@mail.utoronto.ca>
2015-09-05 18:32:57 -0400

    Starting fresh...

--- AdaBoost.hs
+++ AdaBoost.hs
@@ -1,19 +1,1 @@
 module AdaBoost where
-
--- l == n
-initializeNegativeWeights l n = take n repeat (1 / (2 * l))
-
--- m == p
-initializePositiveWeights m p = take p repeat(1 / (2 * p))
-
-normalizeWeights n p = map( / (sum $ n ++ p)) $ n ++ p
-
-weakThresholdClassifier threshold feature polarity = if polarity * feature < threshold
-                                                     then 1
-                                                     else 0
-
-computeError threshold feature polarity x y weight = sum(weight * abs $ weakThresholdClassifier threshold feature polarity - y)
-
-strongThresholdClassifier alpha h = if sum(zipWith(*) alpha h)  > 0.5 * sum(alpha)
-                                    then 1
-                                    else 0

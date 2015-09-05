dfded20b2e9f557fc8bc1bc829e7ec9732d38ee7
Omar Abdeldayem <omar.abdeldayem@mail.utoronto.ca>
2015-09-05 19:13:37 -0400

    Adding weight initialization and normalization

--- AdaBoost.hs
+++ AdaBoost.hs
@@ -1,1 +1,17 @@
 module AdaBoost where
+
+import Data.Matrix
+
+type TData = [(Matrix Int, Int)]
+
+data WeakH = WeakH Float Int Feature
+
+data Feature = Int Int Int Int Int
+
+initWeight :: Int -> Int -> Int -> Float
+initWeight n p y = if y == 1
+                   then 1 / fromIntegral(2 * p)
+                   else 1 / fromIntegral(2 * n)
+
+initWeights :: TData -> Int -> Int ->[Float]
+initWeights ds n p = map (\x -> initWeight n p $ snd x) ds
